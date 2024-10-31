642e589c1155f58ee9aa6908aff9332161622af0
gayubrw27 <joyyshelby69@gmail.com>
2024-11-01 05:22:25 +0700

    add collection route

--- vue/src/components/navigation/navbar/partials/MenuConfig.js
+++ vue/src/components/navigation/navbar/partials/MenuConfig.js
@@ -5,19 +5,241 @@ export const menuConfig = [
         path: '/collections',
         items: [
             {
-                id: 'PROJECT DYSTOPIAN',
+                id: 'project-dystopian',
                 label: 'PROJECT DYSTOPIAN',
                 path: '/collections/project-dystopian',
+                products: [
+                    {
+                        id: 'dystopian-1',
+                        name: 'Project DYSTOPIAN - Spectrum',
+                        price: 'Rp396.000',
+                        categories: 'Tops',
+                        image: '../dystopian1.jpg',
+                        sizes: ['S', 'M', 'L', 'XL', '2XL'],
+                        description: 'Project DYSTOPIAN - Spectrum (Tees)...',
+                    },
+                    {
+                        id: 'dystopian-2',
+                        name: 'Project DYSTOPIAN - Purpureal',
+                        price: 'Rp196.000',
+                        categories: 'Tops',
+                        image: '../dystopian2.jpg',
+                        sizes: ['S', 'M', 'L', 'XL', '2XL'],
+                        description: `Project DYSTOPIAN - Purpureal (Baby Tee)
+
+                                    Width x Length x Sleeve
+
+                                    S: 40 x 39 x 19
+                                    M :42 x 41 x 20
+                                    L : 44 x 43 x 21
+                                    XL : 46 x 45 x 22
+                                    XXL : 48 x 47 x 24
+
+
+                                    *Consider 1 - 2 cm sizing tolerance due to manual measurement.
+
+                                    *Everything that the Empire constructs have passed prudent QUALITY CHECKs. However, in extreme cases where you found errors in our creation, please don't hesitate to submit your complaints to our admin before you rate our masterpieces.`,
+                    },
+                    {
+                        id: 'dystopian-3',
+                        name: 'Project DYSTOPIAN - Mutation',
+                        price: 'Rp196.000',
+                        categories: 'Accessories',
+                        image: '../dystopian3.jpg',
+                        sizes: ['38', '39', '40', '41', '42', '43'],
+                        description: `Project DYSTOPIAN - Mutation (Slides)
+
+                                    Size Chart:
+
+                                    38 / 7US / 25CM
+                                    39 / 7.5US / 25.5CM
+                                    40 / 8US / 26CM
+                                    41 / 9US / 27CM
+                                    42 / 9.5US / 27.5CM
+                                    43 / 10US / 28CM
+
+                                    *Everything that the Empire constructs have passed prudent QUALITY CHECKs. However, in extreme cases where you found errors in our creation, please don't hesitate to submit your complaints to our admin before you rate our masterpieces.`,
+                    },
+                    {
+                        id: 'dystopian-4',
+                        name: 'Project DYSTOPIAN - Umbra',
+                        price: 'Rp96.000',
+                        categories: 'Accessories',
+                        image: '../dystopian4.jpg',
+                        sizes: ['Mid Calf', 'Quarter'],
+                        description: `Project DYSTOPIAN - Umbra (Socks)
+
+                                    One size
+
+                                    - Mid Calf
+                                    - Quarter
+
+                                    *Everything that the Empire constructs have passed prudent QUALITY CHECKs. However, in extreme cases where you found errors in our creation, please don't hesitate to submit your complaints to our admin before you rate our masterpieces.`,
+                    },
+                    // More products here...
+                ],
             },
             {
-                id: 'CATHARSIS EMPIRE',
+                id: 'catharsis-empire',
                 label: 'CATHARSIS EMPIRE',
                 path: '/collections/catharsis-empire',
+                products: [
+                    {
+                        id: 'catharsis-empire-1',
+                        name: 'Catharsis EMPIRE - Izanagiryū & Izanamiryū',
+                        price: 'Rp969.000',
+                        categories: 'Jackets',
+                        image: '../catharsisempire1.jpg',
+                        sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+                        description:
+                            'Catharsis EMPIRE - Izanagiryū & Izanamiryū...',
+                    },
+                    {
+                        id: 'catharsis-empire-2',
+                        name: 'Catharsis EMPIRE - Paragon',
+                        price: 'Rp296.000',
+                        categories: 'Accessories',
+                        image: '../catharsisempire2.jpg',
+                        sizes: ['5', '6', '7', '8', '9', '10', '11'],
+                        description: `Catharsis EMPIRE - Paragon (Ring)
+
+                                    Finger Circumference x Ring Circumference
+
+                                    5: 4,7 x 1,39
+                                    6: 4,8 x 1,42
+                                    7: 4,9 x 1,44
+                                    8: 5,0 x 1,47
+                                    9: 5,1 x 1,50
+                                    10: 5,2 x 1,54
+                                    11: 5,3 x 1,58
+
+
+                                    *Everything that the Empire constructs have passed prudent QUALITY CHECKs. However, in extreme cases where you found errors in our creation, please don't hesitate to submit your complaints to our admin before you rate our masterpieces.`,
+                    },
+                    {
+                        id: 'catharsis-empire-3',
+                        name: 'Catharsis EMPIRE - Ryūmura',
+                        price: 'Rp369.000',
+                        categories: 'Tops',
+                        image: '../catharsisempire3.jpg',
+                        sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+                        description: `Catharsis REBORN - Ryūmura (Tees)
+
+                                    Width x Length x Sleeve
+
+                                    S: 50 x 70 x 21
+                                    M : 53 x 72 x 22
+                                    L : 56 x 76 x 23,5
+                                    XL : 59 x 78 x 24
+                                    XXL : 62 x 80 x 25
+
+
+                                    *Consider 1 - 2 cm sizing tolerance due to manual measurement.
+
+                                    *Everything that the Empire constructs have passed prudent QUALITY CHECKs. However, in extreme cases where you found errors in our creation, please don't hesitate to submit your complaints to our admin before you rate our masterpieces.`,
+                    },
+                    {
+                        id: 'catharsis-empire-4',
+                        name: 'Catharsis EMPIRE x 510 - Repertoire',
+                        price: 'Rp888.510',
+                        categories: 'Jumpers',
+                        image: '../catharsisempire4.jpg',
+                        sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+                        description: `Catharsis x 510 - Repertoire (Boxy Hoodie)
+
+                                    Width x Length x Sleeve
+
+                                    S: 55 x 62 x 58
+                                    M: 58 x 65 x 60
+                                    L: 62 x 69 x 62
+                                    XL: 65 x 72 x 64
+                                    XXL: 68 x 74 x 66
+
+
+                                    *Consider 1 - 2 cm sizing tolerance due to manual measurement.
+
+                                    *Everything that the Empire constructs have passed prudent QUALITY CHECKs. However, in extreme cases where you found errors in our creation, please don't hesitate to submit your complaints to our admin before you rate our masterpieces.*Consider 1 - 2 cm sizing tolerance due to manual measurement.`,
+                    },
+                    // More products here...
+                ],
             },
             {
-                id: 'CATHARSIS REBORN',
+                id: 'catharsis-reborn',
                 label: 'CATHARSIS REBORN',
                 path: '/collections/catharsis-reborn',
+                products: [
+                    {
+                        id: 'catharsis-reborn-1',
+                        name: 'Catharsis REBORN - Adieu',
+                        price: 'Rp969.000',
+                        categories: 'Jackets',
+                        image: '../catharsisreborn1.jpg',
+                        sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+                        description: 'Catharsis REBORN - Adieu (Varsity)...',
+                    },
+                    {
+                        id: 'catharsis-reborn-2',
+                        name: 'Catharsis REBORN - Misae',
+                        price: 'Rp336.000',
+                        categories: 'Bottoms',
+                        image: '../catharsisreborn2.jpg',
+                        sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+                        description: `Catharsis REBORN - Misae (Unfinished Sweat Shorts)
+
+                                    Size Chart
+
+                                    S: 27-29
+                                    M: 30-32
+                                    L: 32-34
+                                    XL: 34-36
+                                    XXL: 36-38
+
+
+                                    *Consider 1 - 2 cm sizing tolerance due to manual measurement.
+
+                                    *Everything that the Empire constructs have passed prudent QUALITY CHECKs. However, in extreme cases where you found errors in our creation, please don't hesitate to submit your complaints to our admin before you rate our masterpieces.`,
+                    },
+                    {
+                        id: 'catharsis-reborn-3',
+                        name: 'Catharsis REBORN - Apex',
+                        price: 'Rp369.000',
+                        categories: 'Tops',
+                        image: '../catharsisreborn3.jpg',
+                        sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+                        description: `Catharsis REBORN - Apex (Tees)
+
+                                    Width x Length x Sleeve
+
+                                    S: 50 x 70 x 21
+                                    M : 53 x 72 x 22
+                                    L : 56 x 76 x 23,5
+                                    XL : 59 x 78 x 24
+                                    XXL : 62 x 80 x 25
+
+
+                                    *Consider 1 - 2 cm sizing tolerance due to manual measurement.
+
+                                    *Everything that the Empire constructs have passed prudent QUALITY CHECKs. However, in extreme cases where you found errors in our creation, please don't hesitate to submit your complaints to our admin before you rate our masterpieces.`,
+                    },
+                    {
+                        id: 'catharsis-reborn-4',
+                        name: 'Catharsis REBORN - Kibisis',
+                        price: 'Rp296.000',
+                        categories: 'Jumpers',
+                        image: '../catharsisreborn4.jpg',
+                        sizes: ['NO SIZES'],
+                        description: `Catharsis REBORN - Kibisis (Bifold Wallet)
+
+                                    Length x Width x Height
+                                    11cm x 2cm x 9cm
+                                    Genuine Cow Leather
+
+                                    4 slots for cards in total
+                                    2 slots for paper money
+                                    A slot for pictures, preferably life changing moments`,
+                    },
+                    // More products here...
+                ],
             },
         ],
     },
--- vue/src/views/CollectionView.vue
+++ vue/src/views/CollectionView.vue
@@ -1,18 +1,60 @@
 <template>
-    <div class="collections">
-        <h1>Collections</h1>
-        <ul>
-            <li v-for="collection in collections" :key="collection.id">
-                <router-link :to="collection.path">{{
-                    collection.label
-                }}</router-link>
-            </li>
-        </ul>
+    <div class="min-h-screen bg-black p-8">
+        <div class="max-w-7xl mx-auto space-y-10">
+            <!-- Daftar Koleksi dengan Produk -->
+            <div
+                v-for="collection in collections"
+                :key="collection.id"
+                class="collection-section"
+            >
+                <!-- Label Koleksi -->
+                <h2
+                    class="text-xl font-bold text-white uppercase mb-4 text-center tracking-[0.2em]"
+                >
+                    {{ collection.label }}
+                </h2>
+
+                <!-- Daftar Produk untuk Koleksi -->
+                <div
+                    class="product-list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10"
+                >
+                    <router-link
+                        v-for="product in collection.products"
+                        :key="product.id"
+                        :to="collection.path"
+                        class="product-card group flex flex-col items-center cursor-pointer transform transition-all duration-200 hover:scale-[101%]"
+                    >
+                        <div class="image-container mb-4">
+                            <img
+                                :src="product.image"
+                                :alt="product.name"
+                                class="w-full h-auto rounded-lg"
+                            />
+                        </div>
+                        <div class="product-info text-center space-y-1">
+                            <h3 class="text-lg font-semibold text-white">
+                                {{ product.name }}
+                            </h3>
+                            <p
+                                class="text-sm text-gray-400 opacity-0 transform -translate-y-2 transition-all duration-200 group-hover:opacity-100 group-hover:translate-y-0"
+                            >
+                                {{ product.categories }}
+                            </p>
+                            <p
+                                class="text-white font-bold opacity-0 transform -translate-y-2 transition-all duration-200 group-hover:opacity-100 group-hover:translate-y-0"
+                            >
+                                {{ product.price }}
+                            </p>
+                        </div>
+                    </router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import { menuConfig } from '@/components/navigation/navbar/partials/MenuConfig' // Adjust this path
+import { menuConfig } from '@/components/navigation/navbar/partials/MenuConfig'
 
 export default {
     name: 'CollectionsView',
@@ -24,9 +66,3 @@ export default {
     },
 }
 </script>
-
-<style scoped>
-.collections {
-    /* Add styles for the collections page */
-}
-</style>
